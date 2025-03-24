896f1c8ade3c03c118b75b60525d1c20386d4e72
User <user@example.com>
2025-03-25 00:39:55 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  
+    // Getter for diameter
+    get diameter() {
+      return this.radius * 2;
+    }
+  
+    // Setter for diameter
+    set diameter(diameter) {
+      this.radius = diameter / 2;
+    }
+  
+    // Getter for circumference
+    get circumference() {
+      return Math.PI * this.diameter;
+    }
+  
+    // Setter for circumference
+    set circumference(circumference) {
+      this.radius = circumference / (Math.PI * 2);
+    }
+  
+    // Getter for area
+    get area() {
+      return Math.PI * this.radius ** 2;
+    }
+  
+    // Setter for area
+    set area(area) {
+      this.radius = Math.sqrt(area / Math.PI);
+    }
+  }
+  
+  module.exports = Circle;
+  
